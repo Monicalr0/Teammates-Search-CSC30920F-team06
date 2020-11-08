08dd34e42c1f18bfacfed12f16ba8cca2bf8a3a3
Rui Zhu <zhuruisy@gmail.com>
2020-11-07 21:15:00 -0500

    finish login.js

---
+++ login.js
@@ -0,0 +1,56 @@
+const login_form = document.querySelector('#form-signin');
+
+login_form.addEventListener('submit', Login_process);
+
+
+function Login_process(e){
+    e.preventDefault();
+    //get the input
+    const input_email = login_form.querySelector('#InputEmail1').value
+    const input_pass = login_form.querySelector('#InputPassword1').value
+    console.log(input_email,input_pass)
+
+    // here would have a server call to get the json from api
+    //in phase 1 i will just hard code here
+    const userDB={
+        "user1@zzz.com" : "123123",
+        "user2@zzz.com" : "1212"
+    }
+    const adminDB={
+        "user3@zzz.com" : "1212"
+    }
+
+    //check if the input is in userDb
+    if (userDB.hasOwnProperty(input_email))
+    {
+        //check if the password is correct
+        if (input_pass == userDB[input_email] )
+        {
+            //avaliable user turn to profile page
+            window.location.href = "index.html";
+        }
+        else
+        {
+            $('#invalid_alert').show()
+            setTimeout(function(){$('#invalid_alert').hide()} ,2500)
+        }
+    }
+    if(adminDB.hasOwnProperty(input_email))
+    {
+        if (input_pass == adminDB[input_email] )
+        {
+            //avaliable user turn to profile page
+            window.location.href = "index.html";
+        }
+        else
+        {
+            $('#invalid_alert').show()
+            setTimeout(function(){$('#invalid_alert').hide()} ,2500)
+        }
+    }
+    else
+    {
+        $('#invalid_alert').show()
+        setTimeout(function(){$('#invalid_alert').hide()} ,2500)
+    }
+}
