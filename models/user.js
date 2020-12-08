dd08c1b9db4fbc3a34729ede27e761ae7c235416
Monicalr0 <mmymonicalr@gmail.com>
2020-12-08 18:27:04 -0500

    fixed some errors.

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     username:{
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const log = console.log
 
 const express = require('express')
+const path = require('path')
 const port = process.env.PORT || 3000
 
 const app = express();
@@ -41,3 +42,6 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
+app.listen(port, () => {
+	log(`Listening on port ${port}...`)
+});
