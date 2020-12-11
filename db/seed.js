45077518d096cd6601576881c13d334d794470db
Monicalr0 <mmymonicalr@gmail.com>
2020-12-11 18:20:16 -0500

    Changed data in seed.js

--- db/seed.js
+++ db/seed.js
@@ -16,17 +16,20 @@ function seed(){
 
     /** USERS **/
 	const users = [
-        {"username": "user", "password": "user", "About": "Hello, I'm user.", "Rate": 9.4,
-        "Language": "English", "Level": "Gold", "PlayStyle": "Aggressive", "PlayTime": 10, "ReportedTime": 1,
-        "PlayedGame": ["PUBG"]
+        {username: "user", password: "user", About: "Hello, I'm user.", Rate: 8,
+        Language: "English", Level: "Gold", PlayStyle: "Aggressive", PlayTime: 10, ReportedTime: 1,
+        PlayedGame: ["PUBG"], MessageReceived: [{"content":"Hello, I'm interesting to form team with you, please dm me if you are interested.",
+        username:"user2", time:"2020/10/31"}], "CommentReceived": [{content: "Awesome Teammate!", username:"User2",
+        time: "2020/11/01", rate:9}, {content: "Would never play with him again.", username:"User3",
+        time: "2020/10/24", rate:1}],
         },
-        {"username": "user2", "password": "user2", "About": "Hello, I'm user2.", "Rate": 9.8,
-        "Language": "English", "Level": "Silver", "PlayStyle": "Aggressive", "PlayTime": 3, "ReportedTime":0,
-        "PlayedGame": ["PUBG"]
+        {username: "user2", password: "user2", About: "Hello, I'm user2.", Rate: 9.8,
+        Language: "English", Level: "Silver", PlayStyle: "Aggressive", PlayTime: 3, "ReportedTime":0,
+        PlayedGame: ["PUBG"], MessageReceived:[], CommentReceived: [], Status:"Normal"
         },
-        {"username": "user3", "password": "user3", "About": "Hello, I'm user3.", "Rate": 2,
-        "Language": "English", "Level": "Platinum", "PlayStyle": "Defensive", "PlayTime": 20, "ReportedTime":1,
-        "PlayedGame": ["PUBG"]
+        {username: "user3", password: "user3", About: "Hello, I'm user3.", Rate: 2,
+        Language: "English", Level: "Platinum", PlayStyle: "Defensive", PlayTime: 20, ReportedTime:1,
+        PlayedGame: ["PUBG"], MessageReceived:[], CommentReceived: [], Status:"Normal"
         }
     ]
     for (user of users) {
